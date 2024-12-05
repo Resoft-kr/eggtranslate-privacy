44b50ecf34c111293062ed0458a7db7dc1dc6d14
dongyoung-tech <ekzmgjalt55@gmail.com>
2024-12-05 17:26:44 +0900

    다국어 반영

---
+++ index.js
@@ -0,0 +1,44 @@
+function getQueryParam(param) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(param);
+}
+
+const language = getQueryParam('lan');
+let htmlFile;
+
+switch (language) {
+    case 'fr':
+        htmlFile = 'fr.html';
+        break;
+    case 'ru':
+        htmlFile = 'ru.html';
+        break;
+    case 'zh':
+        htmlFile = 'zh.html';
+        break;
+    case 'vi':
+        htmlFile = 'vi.html';
+        break;
+    case 'en':
+        htmlFile = 'en.html';
+        break;
+    default:
+        htmlFile = 'ko.html';
+        break;
+}
+
+
+fetch(htmlFile)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('약관을 불러오는데 실패하였습니다');
+        }
+        return response.text();
+    })
+    .then(html => {
+        document.querySelector('body').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('약관 을 불러오는데 실패하였습니다:', error);
+        document.querySelector('body').innerText = '약관을 불러오는데 실패하였습니다.';
+    });
